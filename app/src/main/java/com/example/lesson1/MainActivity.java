72ab75bb424ec123e81d706bc450cd40255a1416
todian-tornero <todanexus@gmail.com>
2021-03-09 20:49:10 +0300

    Lesson 2: Добавил обработчик нажатия на кнопку

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButtonOpen(View view){
+        TextView viewNumber = findViewById(R.id.viewNumber);
+        viewNumber.setText("Number one");
     }
 }
